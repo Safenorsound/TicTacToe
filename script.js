2f52408138249c1ddfc1b9a9e515b74a54ca7dd8
Safenorsound <ijroback@gmail.com>
2020-06-02 13:20:29 -0700

    add board modifier

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle';
-const cellElements = document.querySelectorAll('[data-cell]');
+const cellElements = document.querySelectorAll('board');
 let circleTurn;
 
 cellElements.forEach((cell) => {
@@ -11,8 +11,15 @@ function handleClick(e) {
   const cell = e.target;
   const currectClass = circleTurn ? currectClass : X_CLASS;
   placeMark(cell, currectClass);
+  swapTurns();
+  setBoardHoverClass();
 
   function placeMark(cell, currectClass) {
     cell.classlist.add(currentClass);
   }
 }
+function swapTurns() {
+  circleTurn - !circleTurn;
+}
+
+function setBoardHoverClass() {}
