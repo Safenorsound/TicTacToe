100279d8f90e60e189f1e445dccef53ae1f602e2
Safenorsound <ijroback@gmail.com>
2020-06-03 15:14:46 -0700

    correct winning Message Element line 58

--- script.js
+++ script.js
@@ -12,64 +12,63 @@ const winning_Combinations = [
 ];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
-const winningMessageElement = document.getElementByID ('winningMessage')
-const restartButton = document.getElementById('restartButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+const winningMessageElement = document.getElementByID('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector(
+  '[data-winning-message-text]'
+);
 let circleTurn;
 
 startGame();
 
-restartButton.addEventListener('click', startGame)
+restartButton.addEventListener('click', startGame);
 
 function startGame() {
   circleTurn = false;
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(CIRCLE_CLASS)
-    cell.removeEventListener('click, handleClick')
+  cellElements.forEach((cell) => {
+    cell.classList.remove(X_CLASS);
+    cell.classList.remove(CIRCLE_CLASS);
+    cell.removeEventListener('click, handleClick');
     cell.addEventListener('click', handleClick, { once: true });
   });
   setBoardHoverClass();
-  winningMessageElement.classList.remove('show')
+  winningMessageElement.classList.remove('show');
 }
-
-
-
 
 function handleClick(e) {
   const cell = e.target;
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+
   placeMark(cell, currentClass);
 
-  if (checkWin(currentClass)){
-  endGame(false);
-} else if (isDraw()) {
-  endGame(true)
-} 
-
-  function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass);
+  if (checkWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    swapTurns();
+    setBoardHoverClass();
   }
 }
 
 function endGame(draw) {
   if (draw) {
-    winningMessageElement.innerText = "Draw!"
+    winningMessageElement.innerText = 'Draw!';
   } else {
-    winningMessageTextElement.innerText = '${circleTurn ? "O's" : "X's"} Wins!'
-}
-winningMessageElement.classList.add('show')
-}
-function isDraw () {
-  return [...cellElements].every(cell => {
-    return cell.classList.contains(X_CLASS || cell.classList.contains(CIRCLE_CLASS))
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
   }
-    
-    )
+  winningMessageElement.classList.add('show');
+}
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains(
+      X_CLASS || cell.classList.contains(CIRCLE_CLASS)
+    );
+  });
 }
 
 function placeMark(cell, currentClass) {
-  cell.classList.add(currentClass)
+  cell.classList.add(currentClass);
 }
 
 function swapTurns() {
